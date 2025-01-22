0b4170f290861faf716690039e505f1c74c0dcbd
Sameul Hasan <147124774+sameul-hasan@users.noreply.github.com>
2025-01-22 12:05:37 +0600

    Update main_agent.py

--- main_agent.py
+++ main_agent.py
@@ -1,18 +1,21 @@
 import json
 import httpx
+from rich.console import Console 
+from rich.panel import Panel
+from rich.json import JSON 
 from bs4 import BeautifulSoup
 from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.googlesearch import GoogleSearch 
 from phi.tools.duckduckgo import DuckDuckGo
+from phi.storage.agent.sqlite import SqlAgentStorage
 from dotenv import load_dotenv
-import openai
-import os
 import time
 
 # Load environment variables
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+SESSION_ID = "xxxx-xxxx-xxxx-xxxx"
+MAX_HISTORY_LENGTH = 15 
 
 # DuckDuckGo agent
 duck_agent = Agent(
@@ -94,8 +97,44 @@ def safe_duckduckgo_search(query: str) -> str:
 multi_ai_agent = Agent(
     team=[duck_agent, google_agent, club_agent],
     model=Groq(id="llama-3.3-70b-specdec"),
+    storage=SqlAgentStorage(table_name="agent_sessions", db_file="tmp/agent_storage.db"),
+    add_history_to_messages=True,
+    num_history_responses=5,
     instructions=["You are an agentic AI of Daffodil International University.", "Always provide information based on Daffodil International University data."],
     markdown=True, 
 )
 
-multi_ai_agent.print_response("How to join Data Science club? ", stream=True)
+console = Console() 
+conversation_history = []
+
+def print_chat_history(agent):
+    console.print( 
+         Panel( 
+             JSON(json.dumps([m.model_dump(include={"role", "content"}) for m in agent.memory.messages]), indent=4),
+             title=f"Chat History for session_id: {agent.session_id}",
+             expand=True, 
+             )
+        )
+def handle_user_query(user_query): 
+    conversation_history.append(user_query) 
+    if len(conversation_history) > MAX_HISTORY_LENGTH:
+        conversation_history.pop(0)
+    if user_query.startswith("/exit"): 
+        print("Conversation End.")
+        return False 
+    elif user_query.startswith("/clear"):
+        conversation_history.clear() 
+        print("Conversation history cleared.")
+        return True 
+    elif user_query.startswith("/history"): 
+        print("Conversation History:")
+        for i, query in enumerate(conversation_history):
+            print(f"{i+1}. {query}")
+            return True
+    else:
+        multi_ai_agent.print_response(user_query)
+        return True
+while True: 
+    user_query = input("You: ")
+    if not handle_user_query(user_query):
+        break
